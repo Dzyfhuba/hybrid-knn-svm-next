b5776e8b9025166894a90da6b1e6e5fe522e9d97
Dzyfhuba <hafidz21ub@gmail.com>
2025-01-25 18:28:10 +0700

    feat: Enhance air quality form with ISPU calculations and recommendations

--- src/components/contents/form.tsx
+++ src/components/contents/form.tsx
@@ -2,6 +2,11 @@
 
 import { Form, InputNumber, Modal, notification } from 'antd'
 import { useEffect, useState } from 'react'
+import DebounceSelect from '../debounce-select'
+import ISPU from '@/models/ispu'
+import { Criterion } from '@/types/criterion'
+import katex from 'katex'
+import 'katex/dist/katex.min.css'
 
 interface DataType {
   id: number;
@@ -25,6 +30,39 @@ const ModalForm = ({ open, onCancel, onCreate, editData }: ModalCreateProps) => 
   const [form] = Form.useForm()
   const [notify, notificationContext] = notification.useNotification()
   const [loading, setLoading] = useState(false)
+  const [kualitasRecomendation, setKualitasRecomendation] = useState('')
+  const [ispu, setIspu] = useState<{
+    [key in Criterion]?: {
+      value?: number;
+      label?: string;
+      formula?: string;
+    }
+  }>({})
+  const criterion: { key: Criterion; label: string; unit: string }[] = [{
+    key: 'pm10',
+    label: 'PM10',
+    unit: 'µg/m³',
+  }, {
+    key: 'pm2_5',
+    label: 'PM2.5',
+    unit: 'µg/m³',
+  }, {
+    key: 'so2',
+    label: 'SO2',
+    unit: 'µg/m³',
+  }, {
+    key: 'co',
+    label: 'CO',
+    unit: 'mg/m³',
+  }, {
+    key: 'o3',
+    label: 'O3',
+    unit: 'µg/m³',
+  }, {
+    key: 'no2',
+    label: 'NO2',
+    unit: 'µg/m³',
+  }]
 
   const handleCreate = async () => {
     if (loading) return
@@ -91,43 +129,20 @@ const ModalForm = ({ open, onCancel, onCreate, editData }: ModalCreateProps) => 
         confirmLoading={loading}
         afterClose={() => {
           form.resetFields()
+          setIspu({})
         }}
         modalRender={(modal) => (
           <Form
             form={form}
             labelCol={{ span: 4 }}
-            wrapperCol={{ span: 14 }}
+            wrapperCol={{ style: { width: '100%' } }}
             layout="horizontal"
           >
             {modal}
           </Form>
         )}
       >
-        {[{
-          key: 'pm10',
-          label: 'PM10',
-          unit: 'µg/m³',
-        }, {
-          key: 'pm2_5',
-          label: 'PM2.5',
-          unit: 'µg/m³',
-        }, {
-          key: 'so2',
-          label: 'SO2',
-          unit: 'µg/m³',
-        }, {
-          key: 'co',
-          label: 'CO',
-          unit: 'mg/m³',
-        }, {
-          key: 'o3',
-          label: 'O3',
-          unit: 'µg/m³',
-        }, {
-          key: 'no2',
-          label: 'NO2',
-          unit: 'µg/m³',
-        }].map((field) => (
+        {criterion.map((field) => (
           <Form.Item
             key={field.key}
             name={field.key}
@@ -135,27 +150,79 @@ const ModalForm = ({ open, onCancel, onCreate, editData }: ModalCreateProps) => 
             rules={[
               { required: true, message: `${field.label.toUpperCase()} tidak boleh kosong` },
               { pattern: /^\d+$/, message: `${field.label.toUpperCase()} harus berupa angka` },
+              { pattern: /^[0-9]+$/, message: `${field.label.toUpperCase()} tidak boleh negatif` },
             ]}
+            extra={(
+              <div
+                dangerouslySetInnerHTML={{ __html: ispu[field.key]?.formula || '' }}
+              />
+            )}
           >
-            <InputNumber placeholder={`Masukkan ${field.label.toUpperCase()}`} addonAfter={field.unit} />
+            <InputNumber
+              placeholder={`Masukkan ${field.label.toUpperCase()}`}
+              type='number'
+              addonAfter={field.unit}
+              style={{ width: '100%' }}
+              onKeyUp={(e) => {
+                const value = Number(e.currentTarget.value ?? 0)
+                const {
+                  indexLower,
+                  indexUpper,
+                  concentrationLower,
+                  concentrationUpper,
+                } = ISPU.getStandard(value, field.key)
+
+                const result = ISPU.calculate(value, field.key)
+
+                const formula = `
+                  \\text{ISPU} = \\frac{(\\text{${indexUpper}} - \\text{${indexLower}})}{(\\text{${concentrationUpper}} - \\text{${concentrationLower}})}
+                  \\times (\\text{${value}} - \\text{${concentrationLower}}) + \\text{${indexLower}}
+                  \\\\
+                  \\text{ISPU} = \\text{${result}}
+                  \\\\
+                  \\text{Kualitas} = \\text{${ISPU.getLabel(result)}}
+                `
+                const renderedFormula = katex.renderToString(formula, {throwOnError: false})
+
+                setIspu((prev) => ({
+                  ...prev,
+                  [field.key]: {
+                    value: value,
+                    label: ISPU.getLabel(value),
+                    formula: renderedFormula,
+                  },
+                }))
+
+                setKualitasRecomendation(ISPU.calculateSummaryWithLabel({
+                  pm10: form.getFieldValue('pm10') ?? 0,
+                  pm2_5: form.getFieldValue('pm2_5') ?? 0,
+                  so2: form.getFieldValue('so2') ?? 0,
+                  co: form.getFieldValue('co') ?? 0,
+                  o3: form.getFieldValue('o3') ?? 0,
+                  no2: form.getFieldValue('no2') ?? 0,
+                }))
+              }}
+            />
           </Form.Item>
         ))}
 
-        {/* <Form.Item
+        <Form.Item
           name="kualitas"
           label="Kualitas"
           rules={[{ required: true, message: 'Kualitas tidak boleh kosong' }]}
+          extra={kualitasRecomendation ? (
+            <div>
+              <p>
+                Rekomendasi kualitas udara: <strong>{kualitasRecomendation}</strong>
+              </p>
+            </div>
+          ) : <></>}
         >
-          <Select
-            placeholder="Pilih Kualitas"
-            optionFilterProp='label'
-            showSearch
-            options={['BAIK', 'SEDANG', 'TIDAK SEHAT'].map((kualitas) => ({
-              label: kualitas,
-              value: kualitas,
-            }))}
+          <DebounceSelect
+            route="/api/kualitas"
+            placeholder="Pilih kualitas udara"
           />
-        </Form.Item> */}
+        </Form.Item>
       </Modal>
     </>
   )
--- src/models/ispu.tsx
+++ src/models/ispu.tsx
@@ -1,7 +1,68 @@
+import type { Criterion, Kualitas } from '@/types/criterion'
+
 /**
  * Class representing the ISPU (Indeks Standar Pencemar Udara) model.
  */
 class ISPU {
+  static standards = {
+    pm10: [
+      { indexLower: 0, indexUpper: 50, concentrationLower: 0, concentrationUpper: 50 },
+      { indexLower: 50, indexUpper: 100, concentrationLower: 50, concentrationUpper: 150 },
+      { indexLower: 100, indexUpper: 200, concentrationLower: 150, concentrationUpper: 350 },
+      { indexLower: 200, indexUpper: 300, concentrationLower: 350, concentrationUpper: 420 },
+      { indexLower: 300, indexUpper: null, concentrationLower: 420, concentrationUpper: 500, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 420, alternativeConcenUpper: 500 },
+      { indexLower: null, indexUpper: null, concentrationLower: 500, concentrationUpper: null,  alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 420, alternativeConcenUpper: 500 },
+    ],
+    pm2_5: [
+      { indexLower: 0, indexUpper: 50, concentrationLower: 0, concentrationUpper: 15.5 },
+      { indexLower: 50, indexUpper: 100, concentrationLower: 15.5, concentrationUpper: 55.4 },
+      { indexLower: 100, indexUpper: 200, concentrationLower: 55.4, concentrationUpper: 150.4 },
+      { indexLower: 200, indexUpper: 300, concentrationLower: 150.4, concentrationUpper: 250.4 },
+      { indexLower: 300, indexUpper: null, concentrationLower: 250.4, concentrationUpper: 500, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 250.4, alternativeConcenUpper: 500 },
+      { indexLower: null, indexUpper: null, concentrationLower: 500, concentrationUpper: null, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 250.4, alternativeConcenUpper: 500 },
+    ],
+    so2: [
+      { indexLower: 0, indexUpper: 50, concentrationLower: 0, concentrationUpper: 52 },
+      { indexLower: 50, indexUpper: 100, concentrationLower: 52, concentrationUpper: 180 },
+      { indexLower: 100, indexUpper: 200, concentrationLower: 180, concentrationUpper: 400 },
+      { indexLower: 200, indexUpper: 300, concentrationLower: 400, concentrationUpper: 800 },
+      { indexLower: 300, indexUpper: null, concentrationLower: 800, concentrationUpper: 1200, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 800, alternativeConcenUpper: 1200 },
+      { indexLower: null, indexUpper: null, concentrationLower: 1200, concentrationUpper: null,  alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 800, alternativeConcenUpper: 1200 },
+    ],
+    co: [
+      { indexLower: 0, indexUpper: 50, concentrationLower: 0, concentrationUpper: 8 },
+      { indexLower: 50, indexUpper: 100, concentrationLower: 8, concentrationUpper: 13 },
+      { indexLower: 100, indexUpper: 200, concentrationLower: 13, concentrationUpper: 25 },
+      { indexLower: 200, indexUpper: 300, concentrationLower: 25, concentrationUpper: 40 },
+      { indexLower: 300, indexUpper: null, concentrationLower: 40, concentrationUpper: 60, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 40, alternativeConcenUpper: 60 },
+      { indexLower: null, indexUpper: null, concentrationLower: 60, concentrationUpper: null, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 40, alternativeConcenUpper: 60 },
+    ],
+    // co: [
+    //   { indexLower: 0, indexUpper: 50, concentrationLower: 0, concentrationUpper: 4 },
+    //   { indexLower: 50, indexUpper: 100, concentrationLower: 4, concentrationUpper: 8 },
+    //   { indexLower: 100, indexUpper: 200, concentrationLower: 8, concentrationUpper: 15 },
+    //   { indexLower: 200, indexUpper: 300, concentrationLower: 15, concentrationUpper: 30 },
+    //   { indexLower: 300, indexUpper: null, concentrationLower: 30, concentrationUpper: 45, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 30, alternativeConcenUpper: 45 },
+    //   { indexLower: null, indexUpper: null, concentrationLower: 45, concentrationUpper: null, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 30, alternativeConcenUpper: 45 },
+    // ],
+    o3: [
+      { indexLower: 0, indexUpper: 50, concentrationLower: 0, concentrationUpper: 120 },
+      { indexLower: 50, indexUpper: 100, concentrationLower: 120, concentrationUpper: 235 },
+      { indexLower: 100, indexUpper: 300, concentrationLower: 235, concentrationUpper: 400 },
+      { indexLower: 100, indexUpper: 200, concentrationLower: 400, concentrationUpper: 800 },
+      { indexLower: 300, indexUpper: null, concentrationLower: 800, concentrationUpper: 1000, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 800, alternativeConcenUpper: 1000 },
+      { indexLower: null, indexUpper: null, concentrationLower: 1000, concentrationUpper: null,  alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 800, alternativeConcenUpper: 1000 },
+    ],
+    no2: [
+      { indexLower: 0, indexUpper: 50, concentrationLower: -1, concentrationUpper: 80 },
+      { indexLower: 50, indexUpper: 100, concentrationLower: 80, concentrationUpper: 200 },
+      { indexLower: 100, indexUpper: 200, concentrationLower: 200, concentrationUpper: 1130 },
+      { indexLower: 200, indexUpper: 300, concentrationLower: 1130, concentrationUpper: 2260 },
+      { indexLower: 300, indexUpper: null, concentrationLower: 2260, concentrationUpper: 3000, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 2260, alternativeConcenUpper: 3000 },
+      { indexLower: null, indexUpper: null, concentrationLower: 3000, concentrationUpper: null, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 2260, alternativeConcenUpper: 3000 },
+    ]
+  }
+
   /**
    * Calculates the ISPU (Indeks Standar Pencemar Udara) based on given parameters.
    *
@@ -23,66 +84,9 @@ class ISPU {
       throw new Error('Concentration cannot be negative.')
     }
   
-    const standards = {
-      pm10: [
-        { indexLower: 0, indexUpper: 50, concentrationLower: 0, concentrationUpper: 50 },
-        { indexLower: 50, indexUpper: 100, concentrationLower: 50, concentrationUpper: 150 },
-        { indexLower: 100, indexUpper: 200, concentrationLower: 150, concentrationUpper: 350 },
-        { indexLower: 200, indexUpper: 300, concentrationLower: 350, concentrationUpper: 420 },
-        { indexLower: 300, indexUpper: null, concentrationLower: 420, concentrationUpper: 500, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 420, alternativeConcenUpper: 500 },
-        { indexLower: null, indexUpper: null, concentrationLower: 500, concentrationUpper: null,  alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 420, alternativeConcenUpper: 500 },
-      ],
-      pm2_5: [
-        { indexLower: 0, indexUpper: 50, concentrationLower: 0, concentrationUpper: 15.5 },
-        { indexLower: 50, indexUpper: 100, concentrationLower: 15.5, concentrationUpper: 55.4 },
-        { indexLower: 100, indexUpper: 200, concentrationLower: 55.4, concentrationUpper: 150.4 },
-        { indexLower: 200, indexUpper: 300, concentrationLower: 150.4, concentrationUpper: 250.4 },
-        { indexLower: 300, indexUpper: null, concentrationLower: 250.4, concentrationUpper: 500, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 250.4, alternativeConcenUpper: 500 },
-        { indexLower: null, indexUpper: null, concentrationLower: 500, concentrationUpper: null, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 250.4, alternativeConcenUpper: 500 },
-      ],
-      so2: [
-        { indexLower: 0, indexUpper: 50, concentrationLower: 0, concentrationUpper: 52 },
-        { indexLower: 50, indexUpper: 100, concentrationLower: 52, concentrationUpper: 180 },
-        { indexLower: 100, indexUpper: 200, concentrationLower: 180, concentrationUpper: 400 },
-        { indexLower: 200, indexUpper: 300, concentrationLower: 400, concentrationUpper: 800 },
-        { indexLower: 300, indexUpper: null, concentrationLower: 800, concentrationUpper: 1200, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 800, alternativeConcenUpper: 1200 },
-        { indexLower: null, indexUpper: null, concentrationLower: 1200, concentrationUpper: null,  alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 800, alternativeConcenUpper: 1200 },
-      ],
-      co: [
-        { indexLower: 0, indexUpper: 50, concentrationLower: 0, concentrationUpper: 8 },
-        { indexLower: 50, indexUpper: 100, concentrationLower: 8, concentrationUpper: 13 },
-        { indexLower: 100, indexUpper: 200, concentrationLower: 13, concentrationUpper: 25 },
-        { indexLower: 200, indexUpper: 300, concentrationLower: 25, concentrationUpper: 40 },
-        { indexLower: 300, indexUpper: null, concentrationLower: 40, concentrationUpper: 60, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 40, alternativeConcenUpper: 60 },
-        { indexLower: null, indexUpper: null, concentrationLower: 60, concentrationUpper: null, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 40, alternativeConcenUpper: 60 },
-      ],
-      // co: [
-      //   { indexLower: 0, indexUpper: 50, concentrationLower: 0, concentrationUpper: 4 },
-      //   { indexLower: 50, indexUpper: 100, concentrationLower: 4, concentrationUpper: 8 },
-      //   { indexLower: 100, indexUpper: 200, concentrationLower: 8, concentrationUpper: 15 },
-      //   { indexLower: 200, indexUpper: 300, concentrationLower: 15, concentrationUpper: 30 },
-      //   { indexLower: 300, indexUpper: null, concentrationLower: 30, concentrationUpper: 45, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 30, alternativeConcenUpper: 45 },
-      //   { indexLower: null, indexUpper: null, concentrationLower: 45, concentrationUpper: null, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 30, alternativeConcenUpper: 45 },
-      // ],
-      o3: [
-        { indexLower: 0, indexUpper: 50, concentrationLower: 0, concentrationUpper: 120 },
-        { indexLower: 50, indexUpper: 100, concentrationLower: 120, concentrationUpper: 235 },
-        { indexLower: 100, indexUpper: 300, concentrationLower: 235, concentrationUpper: 400 },
-        { indexLower: 100, indexUpper: 200, concentrationLower: 400, concentrationUpper: 800 },
-        { indexLower: 300, indexUpper: null, concentrationLower: 800, concentrationUpper: 1000, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 800, alternativeConcenUpper: 1000 },
-        { indexLower: null, indexUpper: null, concentrationLower: 1000, concentrationUpper: null,  alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 800, alternativeConcenUpper: 1000 },
-      ],
-      no2: [
-        { indexLower: 0, indexUpper: 50, concentrationLower: -1, concentrationUpper: 80 },
-        { indexLower: 50, indexUpper: 100, concentrationLower: 80, concentrationUpper: 200 },
-        { indexLower: 100, indexUpper: 200, concentrationLower: 200, concentrationUpper: 1130 },
-        { indexLower: 200, indexUpper: 300, concentrationLower: 1130, concentrationUpper: 2260 },
-        { indexLower: 300, indexUpper: null, concentrationLower: 2260, concentrationUpper: 3000, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 2260, alternativeConcenUpper: 3000 },
-        { indexLower: null, indexUpper: null, concentrationLower: 3000, concentrationUpper: null, alternativeLower: 200, alternativeUpper: 300, alternativeConcenLower: 2260, alternativeConcenUpper: 3000 },
-      ]
-    }
+    
   
-    const standard = standards[element].find(({ concentrationLower, concentrationUpper }) => {
+    const standard = this.standards[element].find(({ concentrationLower, concentrationUpper }) => {
       if (concentrationUpper === null) {
         return real >= concentrationLower
       }
@@ -109,7 +113,37 @@ class ISPU {
   
     return result
   }
+
+  static getStandard(real: number, element: 'pm10' | 'pm2_5' | 'so2' | 'co' | 'o3' | 'no2') {
+    const standard = this.standards[element].find(({ concentrationLower, concentrationUpper }) => {
+      if (concentrationUpper === null) {
+        return real >= concentrationLower
+      }
+      return real >= concentrationLower && real <= concentrationUpper
+    })
+
+    if (!standard) {
+      throw new Error(`Concentration ${real} is not within the bounds for ${element}.`)
+    }
+
+    let { indexLower, indexUpper, concentrationLower, concentrationUpper } = standard
+    const { alternativeLower, alternativeUpper, alternativeConcenLower, alternativeConcenUpper } = standard
   
+    // Use alternative values if necessary
+    if (concentrationUpper === null || indexUpper === null) {
+      indexLower = alternativeLower!
+      indexUpper = alternativeUpper!
+      concentrationLower = alternativeConcenLower!
+      concentrationUpper = alternativeConcenUpper!
+    }
+
+    return {
+      indexLower,
+      indexUpper,
+      concentrationLower,
+      concentrationUpper
+    }
+  }
 
   /**
    * Returns the air quality label based on the given ISPU (Indeks Standar Pencemar Udara) value.
@@ -145,19 +179,14 @@ class ISPU {
    * @param {Object} params.data - An object where keys are pollutant names and values are their concentrations.
    * @returns {string} - The label corresponding to the average concentration.
    */
-  static calculateSummaryWithLabel(data: { [key: string]: number
-  }): 'BAIK' | 'SEDANG' | 'TIDAK SEHAT' | 'SANGAT TIDAK SEHAT' | 'BERBAHAYA' {
+  static calculateSummaryWithLabel(data: Record<Criterion, number>): Kualitas {
     const result = Object.entries(data).map(([element, concentration]) => {
-      console.log('Element:', element)
       const result = ISPU.calculate(concentration, element as 'pm10' | 'pm2_5' | 'so2' | 'co' | 'o3' | 'no2')
-      console.log('Result:', result)
       return result
     })
     
     // get largest value
     const max = Math.max(...result)
-    console.log('Max:', max)
-    console.log('Largest result:', result.indexOf(max))
     return ISPU.getLabel(max)
   }
 }
