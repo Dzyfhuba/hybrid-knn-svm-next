384105dfe28869069fc7bd1d4cdcee1c42508c3e
Dzyfhuba <hafidz21ub@gmail.com>
2025-01-26 21:18:14 +0700

    feat: Refactor global state model structure and enhance fetch logic with reference handling

--- src/state/store.ts
+++ src/state/store.ts
@@ -3,14 +3,14 @@ import axios from 'axios'
 import { action, Action, createStore, thunk, Thunk } from 'easy-peasy'
 
 export interface GlobalState {
-  model: Database['svm_knn']['Tables']['model']['Row'][]
-  putModel: Action<GlobalState, Database['svm_knn']['Tables']['model']['Row'][]>
-  setModel: Action<GlobalState, Database['svm_knn']['Tables']['model']['Row'][]>
+  model: Database['svm_knn']['Tables']['model']['Insert']
+  putModel: Action<GlobalState, Database['svm_knn']['Tables']['model']['Row']>
+  setModel: Action<GlobalState, ((model: GlobalState['model']) => GlobalState['model']) | GlobalState['model']>
   fetchModel: Thunk<GlobalState, void>
 }
 
 const store = createStore<GlobalState>({
-  model: [],
+  model: {},
   putModel: action((state, payload) => {
     axios.put('/api/model', payload)
       .catch((error) => {
@@ -18,11 +18,11 @@ const store = createStore<GlobalState>({
       })
   }),
   setModel: action((state, payload) => {
-    state.model = payload
+    state.model = typeof payload === 'function' ? payload(state.model) : payload
   }),
   fetchModel: thunk(async (actions) => {
-    const { data } = await axios.get('/api/model')
-    actions.setModel(data)
+    const { data } = await axios.get('/api/model' + (typeof window !== 'undefined' ? `?reference=${window.localStorage.getItem('reference')}` : ''))
+    actions.setModel(data.item)
   })
 })
 
