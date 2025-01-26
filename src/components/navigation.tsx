e96b23329836344453c65a9bfb6c4d1af4106d50
Dzyfhuba <hafidz21ub@gmail.com>
2025-01-26 18:51:57 +0700

    feat: Update Navigation component styles for improved visibility and layering

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -3,11 +3,11 @@ import { Header } from 'antd/es/layout/layout'
 const Navigation = () => {
   return (
     <Header
-      className="h-14 shadow-md flex items-center"
+      className="h-14 shadow-md flex items-center z-10"
     >
       <nav>
         <h1
-          className="text-2xl font-bold text-white"
+          className="text-2xl font-bold dark:text-white"
         >
           {process.env.NEXT_PUBLIC_APP_TITLE}
         </h1>
